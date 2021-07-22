a86c30a32b5c1ac85768dbc7d56ed4edb4043894
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-07-22 22:55:48 +0800

    Changed the styling of the header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function Header(props) {
     return (
-
         <header className="header bg-info">
-            <a href="https://github.com/Shelbyrp"> <h1 className="text-white">Shelby Pignat</h1></a>
+            <h1 className="text-white">Shelby Pignat</h1>
         </header>
 
     );
