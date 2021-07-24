7753df4e8d2b37115e10b6bb7006b789500a8e37
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-07-24 23:09:54 +0800

    removed src.

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return (
-        <header className="header bg-dark">
-            <h1 className="text-white">Shelby Pignat</h1>
-        </header>
-    );
-}
-
-export default Header;
