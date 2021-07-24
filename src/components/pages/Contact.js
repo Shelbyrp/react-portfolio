b475f949452c8999f68d94d9a1a490c001582ed4
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-07-24 17:13:11 +0800

    Got the error message to show for email.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,35 +1,45 @@
 import React, { useState } from 'react';
-import { checkEmail } from '../../utils/helper';
+import { validateEmail } from '../../utils/helper';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Contact() {
-    const [formData, setFormData] = useState({});
-    const [error, setError] = useState('');
+    const [formData, setFormData] = useState({ contactName: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
+    const { contactName, email, message } = formData;
+
     const handlePageForm = (e) => {
         e.preventDefault();
-        console.log(formData);
+        console.log("formdata", formData);
     };
 
     function handleChange(e) {
+        console.log("error message", typeof errorMessage);
         if (e.target.name === 'email') {
-            const emailValid = checkEmail(e.target.value);
-            console.log("email", e.target.value)
-            if (!emailValid) {
-                setError('Your email is required.');
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('please enter a valid email');
             } else {
-                setError('');
+                setErrorMessage('');
             }
         } else {
             if (!e.target.value.length) {
-                setError(`${e.target.name} is required.`);
+                setErrorMessage(`${e.target.name} is required.`);
             } else {
-                setError('');
+                setErrorMessage('');
             }
         }
-        if (!error) {
+        if (e.target.name === 'message') {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
             setFormData({ ...formData, [e.target.name]: e.target.value })
         }
     }
+
     return (
         <div class="main-container">
             <section class="bgimage">
@@ -41,13 +51,13 @@ export default function Contact() {
                 </div>
             </section>
             <div class="container wrapper">
-                <form>
+                <form class="col-lg-10 col-md-10 col-sm-10 col-xs-10 mb-5">
                     <h2>Contact Us</h2>
                     <div>
-                        <label class="col-sm-2" for="name">Name:</label>
+                        <label class="col-sm-2" htmlFor="name">Name:</label>
                         <div class="col-sm-10">
                             <div>
-                                <input name="name" class="form-control" type="text" id="name" onChange={handleChange}></input>
+                                <input name="contactName" class="form-control" type="text" defaultValue={contactName} id="name" onChange={handleChange}></input>
                             </div>
                         </div>
                     </div>
@@ -55,19 +65,19 @@ export default function Contact() {
                         <label class="col-sm-2">Email Address:</label>
                         <div class="col-sm-10">
                             <div>
-                                <input class="form-control" name="Email" type="email" id="email" onChange={handleChange}></input>
+                                <input class="form-control" name="email" type="email" id="email" defaultValue={email} onChange={handleChange}></input>
                             </div>
                         </div>
                     </div>
                     <div>
                         <label class="col-sm-2" >Message:</label>
                         <div>
-                            <textarea class="form-control" name="message" rows="3" onChange={handleChange}></textarea>
+                            <textarea class="form-control" name="message" rows="3" defaultValue={message} onChange={handleChange}></textarea>
                         </div>
                     </div>
-                    {error && (
+                    {errorMessage && (
                         <div>
-                            <p className="error-text">{error}</p>
+                            <p className="error-text">{errorMessage}</p>
                         </div>
                     )}
                     <div class="form-group pull-right">
