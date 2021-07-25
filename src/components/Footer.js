65f81291a5db1b91dc87ff387467c8b82e240ab8
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-07-25 12:15:59 +0800

    Footer links now open in new windows.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ function Footer() {
         <div>
             <footer className="content bg-dark">
                 <div className="footer-copyright text-center text-white py-3">
-                    <a href="https://www.linkedin.com/in/shelby-pignat-08795293/"><img src="https://img.icons8.com/ios-filled/50/ffffff/linkedin.png" alt="LinkedIn" className="icon" /></a>
-                    <a href="https://github.com/Shelbyrp/react-portfolio"><img src="https://img.icons8.com/ios-filled/50/ffffff/github.png" alt="Github" className="icon" /></a>
-                    <a href="https://twitter.com/shelby_pignat"><img src="https://img.icons8.com/material-rounded/50/ffffff/twitter.png" alt="Twitter" className="icon" /></a>
-                    <a href="https://stackoverflow.com/users/14975737/shelby-p"><img src="https://img.icons8.com/ios-filled/50/ffffff/stackoverflow.png" alt="Stack Overflow" className="icon" /></a>
+                    <a href="https://www.linkedin.com/in/shelby-pignat-08795293/" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/ios-filled/50/ffffff/linkedin.png" alt="LinkedIn" className="icon" /></a>
+                    <a href="https://github.com/Shelbyrp/react-portfolio" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/ios-filled/50/ffffff/github.png" alt="Github" className="icon" /></a>
+                    <a href="https://twitter.com/shelby_pignat" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/material-rounded/50/ffffff/twitter.png" alt="Twitter" className="icon" /></a>
+                    <a href="https://stackoverflow.com/users/14975737/shelby-p" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/ios-filled/50/ffffff/stackoverflow.png" alt="Stack Overflow" className="icon" /></a>
                     <p>
                         &copy; Shelby Pignat</p>
                 </div>
